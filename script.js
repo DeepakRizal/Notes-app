618d5880f1d4776db1791347f00856f07e87ee10
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-06 21:38:34 +0530

    feat(notes): enable editing of existing notes

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const categoryError = document.querySelector(".category-error");
 const notesContainer = document.querySelector(".notes-container");
 
 let notesArray = [];
+let editingNoteId = null;
 
 function generateID() {
   return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
@@ -24,7 +25,7 @@ function displayNotes(notes) {
     <div class="note">
     <div class="buttons" >
     <button class="edit" >
-    <img class="edit-icon" src="./assests/edit.png" >
+    <img class="edit-icon" data-id="${note.id}" src="./assests/edit.png" >
     </button>
     <button class="delete">
     <img class="delete-icon" src="./assests/delete.png"
@@ -40,8 +41,6 @@ function displayNotes(notes) {
     `;
     })
     .join("");
-
-  console.log(notesMarkup);
 
   notesContainer.innerHTML = "";
   notesContainer.innerHTML = notesMarkup;
@@ -64,6 +63,12 @@ triggerModalBtn.addEventListener("click", () => {
   writeNoteModal.classList.add("active");
 });
 
+function emptyModal() {
+  noteTitle.value = "";
+  notesBody.value = "";
+  category.value = "";
+}
+
 cancel.addEventListener("click", () => {
   writeNoteModal.classList.remove("active");
 });
@@ -79,39 +84,58 @@ writeNoteModal.addEventListener("click", () => {
 modalform.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const noteTitleValue = noteTitle.value.trim();
-  const notesBodyValue = notesBody.value.trim();
-  const categoryValue = category.value.trim();
+  if (editingNoteId) {
+    const noteIndex = notesArray.findIndex((note) => note.id === editingNoteId);
+    if (noteIndex !== -1) {
+      notesArray[noteIndex].title = noteTitle.value;
+      notesArray[noteIndex].note = notesBody.value;
+      notesArray[noteIndex].category = category.value;
+    }
 
-  notesTitleError.textContent = "";
-  notesbodyError.textContent = "";
-  categoryError.textContent = "";
+    displayNotes(notesArray);
 
-  const erros = {};
+    editingNoteId = null;
+    create.textContent = "Create";
 
-  //form validation logic
-  if (!noteTitleValue) erros.title = "Title cannot be empty!";
-  if (!notesBodyValue) erros.notes = "Notes cannot be empty!";
-  if (!categoryValue) erros.category = "Please select the category!";
+    //emptying the modal
+    emptyModal();
 
-  console.log(erros);
+    writeNoteModal.classList.remove("active");
+  } else {
+    const noteTitleValue = noteTitle.value.trim();
+    const notesBodyValue = notesBody.value.trim();
+    const categoryValue = category.value.trim();
 
-  if (Object.keys(erros).length > 0) {
-    if (erros.title) notesTitleError.textContent = erros.title;
-    if (erros.notes) notesbodyError.textContent = erros.notes;
-    if (erros.category) categoryError.textContent = erros.category;
+    notesTitleError.textContent = "";
+    notesbodyError.textContent = "";
+    categoryError.textContent = "";
 
-    return;
+    const erros = {};
+
+    //form validation logic
+    if (!noteTitleValue) erros.title = "Title cannot be empty!";
+    if (!notesBodyValue) erros.notes = "Notes cannot be empty!";
+    if (!categoryValue) erros.category = "Please select the category!";
+
+    console.log(erros);
+
+    if (Object.keys(erros).length > 0) {
+      if (erros.title) notesTitleError.textContent = erros.title;
+      if (erros.notes) notesbodyError.textContent = erros.notes;
+      if (erros.category) categoryError.textContent = erros.category;
+
+      return;
+    }
+
+    const idValue = generateID();
+
+    createNote(idValue, noteTitleValue, notesBodyValue, categoryValue);
+
+    // empthying the modal
+    emptyModal();
+
+    writeNoteModal.classList.remove("active");
   }
-
-  const idValue = generateID();
-
-  createNote(idValue, noteTitleValue, notesBodyValue, categoryValue);
-  noteTitle.value = "";
-  notesBody.value = "";
-  category.value = "";
-
-  writeNoteModal.classList.remove("active");
 });
 
 //form validation
@@ -137,3 +161,20 @@ handleFormValidation(noteTitle, "blur", notesTitleError, "Title");
 // Apply validation to notesBody
 handleFormValidation(notesBody, "input", notesbodyError, "Notes");
 handleFormValidation(notesBody, "blur", notesbodyError, "Notes");
+
+//edit note and delete note
+notesContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit-icon")) {
+    const elementId = e.target.getAttribute("data-id");
+    const curNote = notesArray.filter((note) => note.id === elementId);
+    noteTitle.value = curNote[0].title;
+    notesBody.value = curNote[0].note;
+    category.value = curNote[0].category;
+
+    editingNoteId = elementId;
+
+    create.textContent = "Update";
+
+    writeNoteModal.classList.add("active");
+  }
+});
