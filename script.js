27a30b4a156690ab9ae9b3bbc5494f44a6b30199
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-06 10:51:37 +0530

    feat: add basic form validation to notes form

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const triggerModalBtn = document.querySelector(".trigger-modal-btn");
 const writeNoteModal = document.querySelector(".write-note-modal");
 const cancel = document.querySelector(".cancel");
 const create = document.querySelector(".create-note");
-const modalContainer = document.querySelector(".modal-form");
+const modalform = document.querySelector(".modal-form");
 const noteTitle = document.querySelector("#title");
 const notesBody = document.querySelector("#notes");
+const notesTitleError = document.querySelector(".notes-title-error");
+const notesbodyError = document.querySelector(".notes-body-error");
+
+const note = {
+  id: null,
+  title: "",
+  note: "",
+};
+
+const notes = [];
+
+function generateID() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+}
 
 triggerModalBtn.addEventListener("click", () => {
   writeNoteModal.classList.add("active");
@@ -14,16 +28,41 @@ cancel.addEventListener("click", () => {
   writeNoteModal.classList.remove("active");
 });
 
-create.addEventListener("click", (e) => {
-  e.preventDefault();
-  writeNoteModal.classList.remove("active");
-  //write the note creation logic below
-});
-
-modalContainer.addEventListener("click", (e) => {
+modalform.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
 writeNoteModal.addEventListener("click", () => {
   writeNoteModal.classList.remove("active");
+});
+
+modalform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //form validation logic
+  if (noteTitle.value.trim() === "" && notesBody.value.trim() === "") {
+    notesTitleError.textContent = "title cannot be empty!";
+    notesbodyError.textContent = "notes cannot be empty!";
+    return;
+  }
+
+  if (noteTitle.value.trim() === "" && notesBody.value.trim() !== "") {
+    notesTitleError.textContent = "title cannot be empty!";
+    return;
+  }
+  if (noteTitle.value.trim() !== "" && notesBody.value.trim() === "") {
+    notesbodyError.textContent = "notes cannot be empty!";
+    return;
+  }
+
+  //   writeNoteModal.classList.remove("active");
+});
+
+//remove the error message when the user starts typing
+noteTitle.addEventListener("input", () => {
+  notesTitleError.textContent = "";
+});
+
+notesBody.addEventListener("input", () => {
+  notesbodyError.textContent = "";
 });
